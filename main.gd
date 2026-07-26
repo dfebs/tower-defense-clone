5721100babbbe5f6391987f9c37ada48c0fea3ff
dfebs <daniel.febles97@gmail.com>
2026-07-25 23:30:40 -0400

    Add a win condition

--- main.gd
+++ main.gd
@@ -10,25 +10,33 @@ func _ready() -> void:
 	$MainMenu.start_game.connect(_on_main_menu_start_game)
 	pass
 
-
 func _process(_delta: float) -> void:
 	pass
-
 
 func _on_main_menu_start_game() -> void:
 	var level = level_class.instantiate()
 	$MainMenu.disable()
 	add_child(level)
 	$Level.lose_game.connect(_lose_game)
+	$Level.win_game.connect(_win_game)
+
+func _end_game(message: String):
+	$Level.queue_free()
+	await _notify(message)
+	$MainMenu.enable()
+	pass
 
 func _lose_game():
-	$Level.queue_free()
+	_end_game("You lose!")
 
+func _win_game():
+	_end_game("You won!")
+
+func _notify(message):
 	var notify = notify_class.instantiate()
+	notify.text = message
 	add_child(notify)
 
 	await get_tree().create_timer(2.5).timeout
 	notify.queue_free()
-
-	$MainMenu.enable()
 
--- tower/level.gd
+++ tower/level.gd
@@ -1,23 +1,37 @@
 extends Node
 
 @export var bad_guy_scene: PackedScene
+const ENEMY_GROUP = "enemies"
 
 signal lose_game
+signal win_game
 
 func _ready() -> void:
+	$SpawnTimer.timeout.connect(_on_spawn_timer_timeout)
+	$LevelTimer.timeout.connect(_on_level_timer_timeout)
 	pass
 
 func _process(_delta: float) -> void:
-	pass
+	if $SpawnTimer.is_stopped():
+		var bad_guys = get_tree().get_nodes_in_group(ENEMY_GROUP).size()
+		if bad_guys == 0:
+			win_game.emit()
 
 func _on_spawn_timer_timeout() -> void:
 	var spawn = [$Spawn1, $Spawn2, $Spawn3].pick_random()
 	var bad_guy = bad_guy_scene.instantiate()
+
 	bad_guy.position = spawn.position
 	bad_guy.z_index = -1
+	bad_guy.add_to_group(ENEMY_GROUP)
+
 	add_child(bad_guy)
 	bad_guy.exited.connect(_lose_game)
 
-func _lose_game():
+func _on_level_timer_timeout() -> void:
+	$SpawnTimer.stop()
+	pass
+
+func _lose_game() -> void:
 	lose_game.emit()
 
