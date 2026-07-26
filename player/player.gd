7c725fb3c11edd3f64d81f88b1187f50d0efff41
dfebs <daniel.febles97@gmail.com>
2026-07-26 16:34:51 -0400

    Add cooldown to weapon

--- player/player.gd
+++ player/player.gd
@@ -9,10 +9,11 @@ func _process(delta: float) -> void:
 	pass
 
 func _input(event):
-	if event.is_action_pressed("fire"):
+	if event.is_action_pressed("fire") && $WeaponCooldown.is_stopped():
 		var direction = (event.position - global_position).normalized()
 		var projectile = projectile_class.instantiate()
 
 		projectile.direction = direction
 
 		add_child(projectile)
+		$WeaponCooldown.start()
